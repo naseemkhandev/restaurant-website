3dcaf23879d675227f1e52486cb0992df8a78252
Naseem Khan <devnaseemkhan@gmail.com>
2024-09-10 01:19:02 +0500

    feat: Add image upload functionality to menu routes

--- server/src/routes/menu.routes.ts
+++ server/src/routes/menu.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import verifyToken from "../middlewares/verifyToken.middleware";
-import { addMenu } from "../controllers/menu.controller";
+import { addMenu, updateMenu } from "../controllers/menu.controller";
+import { upload } from "../middlewares/multer.middleware";
 const router = express.Router();
 
-router.post("/", verifyToken, addMenu);
+router.post("/", verifyToken, upload.single("image"), addMenu);
+router.patch("/:id", verifyToken, upload.single("image"), updateMenu);
 
 export default router;
